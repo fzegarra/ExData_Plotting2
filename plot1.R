1ed84c2c38301baae6d77a9971b80466e920239a
fzegarra <fzegarra@iadb.org>
2014-11-11 17:26:57 -0500

    Course Project 2

---
+++ plot1.R
@@ -0,0 +1,23 @@
+## Course Project No 2. Exploratory Data Analysis. 
+## plot1.R: Have total emissions from PM2.5 decreased in the United States 
+## from 1999 to 2008? Using the base plotting system, make a plot showing the total
+## PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+## 1. Set the working directory when de data was downloaded 
+## and reading the plyr library
+setwd("D:/DataScience/Exploratory Data Analisis/Project_Course_2")
+library(plyr)
+## 2. Unzip the data
+unzip("exdata-data-NEI_data.zip")
+## 3. Read the data: a) Source_Classification_Code.rds; b) summarySCC_PM25.rds
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+## 4. Aggregate the data by years
+data_by_years<-ddply(NEI, c("year"), summarise, Emissions=sum(Emissions, na.rm=TRUE))
+## 5. Generating the png file
+par("mar"=c(5, 4, 4, 1))
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+## 6. Plotting the file.
+plot(data_by_years, type = "l", col="red", xlab = "Year", 
+     main = "Total Emissions in U.S. (from 1999 to 2008)", 
+     ylab = expression('Total PM'[2.5]*" Emission"))
+dev.off()
